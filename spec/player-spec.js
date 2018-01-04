23bbb8edc5ce5365968bdb2a39d0fe7f7f02ff69
Cheryl Frankenfield and Colin Worf <cheryl@>
2018-01-03 16:42:04 -0800

    Add Istanbul coverage

--- spec/player-spec.js
+++ spec/player-spec.js
@@ -2,18 +2,17 @@ var Player = require('./../js/scripts.js').playerModule;
 
 describe('Player', function () {
   it('should test whether the total score gets the temporary score upon pass', function(){
-    var player = new Player("harry", true, 10, 33)
+    var player = new Player("harry", true, 10, 33);
     expect(player.pass()).toEqual(43);
   });
 
   it('should test whether the player changes upon pass', function(){
-    var player = new Player("harry", true, 10, 33)
-    player.pass();
-    expect(player.active).toEqual(true);
+    var player = new Player("harry", true, 10, 33);
+    expect(player.turn(1)).toEqual(false);
   });
 
   it('should test that tempScore is reset to zero if player rolls 1', function() {
-    var player = new Player("harry", true, 10, 33)
+    var player = new Player("harry", true, 10, 33);
     player.turn(1);
     expect(player.tempScore).toEqual(0);
   });
